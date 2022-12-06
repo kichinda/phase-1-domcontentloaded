a8b567e7c6398367b5ad66acf99059e9024fa6e4
kichinda <kichindajesse@gmail.com>
2022-12-06 13:15:01 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Your code goes here
+document.addEventListener("DOMContentLoaded",init);
+document.body.onload=()=>{
+    console.log("Loaded")
+};
+function init() {
+    console.log("Loaded");
+    const main=document.getElementById("text");
+    main.textContent="This is really cool!"
+};
